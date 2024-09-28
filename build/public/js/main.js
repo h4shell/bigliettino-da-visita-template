4f1e9d92183312c976f31470e0b619a06adb57da
Lorenzo Fornara <fw1987@gmail.com>
2024-09-28 07:49:23 +0000

    nascondo URL

--- build/public/js/main.js
+++ build/public/js/main.js
@@ -63,3 +63,8 @@ wallpapers.forEach((el) => {
     el.style.backgroundSize = 'cover';
     el.style.backgroundRepeat = 'no-repeat';
 });
+
+history.replaceState(null, '', `${params.name}`);
+document.title = `${params.name}`
+
+// http://127.0.0.1:5500/build/?name=test&color=lime&type=test&tel=39%20348%2012%2006%20604&email=h4shell@gmail.com&photo=https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP.pn1WcM8XtGEqWC26R6uOOQHaHa%26pid%3DApi&f=1&ipt=b621860fa04adef3cd8288221193ef015e95cfd38be0e1e49cb037bd9f48d0c5&ipo=images&bg=https://images.unsplash.com/photo-1484557052118-f32bd25b45b5?q=80&w=2069&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D
